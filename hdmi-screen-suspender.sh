285424a3515f7db9d1cbf6e7d6ea27f0f148c130
Alberto Vazquez <albertovmrm@gmail.com>
2021-08-11 17:46:05 +0200

    Remove getline function

--- hdmi-screen-suspender.sh
+++ hdmi-screen-suspender.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 DISCONNECTED="disconnected"
 CONNECTED="connected"
 HLS_OFF="#HandleLidSwitch=ignore"
@@ -8,20 +7,6 @@ FILENAME="/etc/systemd/logind.conf"
 
 status1=$(xrandr | grep "HDMI-1")
 first_iteration=true
-
-get_line() {
-    n=0
-    while read line; do
-        if [[ $line == *"HandleLidSwitch="* ]]
-        then
-            break
-        fi
-        n=$((n+1))
-
-    done < $FILENAME
-
-    return $n
-}
 
 modify_file() {
     sed -i "s/$2/$1/" FILENAME
