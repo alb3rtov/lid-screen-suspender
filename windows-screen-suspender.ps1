029ea44fe571d6bc679c4c4a7094367ce967bedf
Alberto <albertovmrm@gmail.com>
2022-08-31 13:13:00 +0200

    Set initial lid configuration

--- windows-screen-suspender.ps1
+++ windows-screen-suspender.ps1
@@ -1,5 +1,18 @@
 $objWMi1 = get-wmiobject -namespace root\WMI -computername localhost -Query "Select * from WmiMonitorConnectionParams"
 
+function Set-Lid-Configuration {
+    
+    if ($objWMi1.Count -lt 2) { # Without external monitors
+        powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
+        powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
+    } else { # With external monitors
+        powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+        powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+    }
+}
+
+Set-Lid-Configuration # Initial configuration
+
 while($true) {
     Start-Sleep -s 5
     $objWMi2 = get-wmiobject -namespace root\WMI -computername localhost -Query "Select * from WmiMonitorConnectionParams"
@@ -7,12 +20,6 @@ while($true) {
     if ($objWMi1.Count -ne $objWMi2.Count) {
         $objWMi1 = $objWMi2
         
-        if ($objWMi2.Count -lt 2) { # Without external monitors
-            powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
-            powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
-        } else { # With external monitors
-            powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-            powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-        }
+        Set-Lid-Configuration
     }
 }
